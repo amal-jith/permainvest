59cb14f6f008119d0b21bf671720f4ca23e32d6a
amal_jith <123450863+amal-jith@users.noreply.github.com>
2026-05-12 22:35:12 +0530

    Update urls.py

--- app_perma/urls.py
+++ app_perma/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('our-firm', views.our_firm, name='our_firm'),
     path('investment-model', views.investment_model, name='investment_model'),
+    path('investment-values', views.investment_values, name='investment_values'),
+    path('ethical-investing', views.ethical_investing, name='ethical_investing'),
+    path('technology-and-innovation-commitment', views.technology_innovation, name='technology_innovation'),
 ]
