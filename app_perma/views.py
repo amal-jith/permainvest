a5bd8e531aab070960acfe93a62e1999327c6f2c
amal_jith <123450863+amal-jith@users.noreply.github.com>
2026-05-12 22:34:58 +0530

    Update views.py

--- app_perma/views.py
+++ app_perma/views.py
@@ -10,3 +10,12 @@ def our_firm(request):
 
 def investment_model(request):
     return render(request, "investment-model.html",)
+
+def investment_values(request):
+    return render(request, "investment-values.html",)
+
+def ethical_investing(request):
+    return render(request, "ethical-investing.html",)
+
+def technology_innovation(request):
+    return render(request, "technology_and_innovation.html",)
